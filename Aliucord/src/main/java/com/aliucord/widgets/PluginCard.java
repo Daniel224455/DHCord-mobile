b7841ae4508190a71e4d0f39b074782e093967ca
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-04-04 20:16:32 +0200

    dhc

--- Aliucord/src/main/java/com/aliucord/widgets/PluginCard.java
+++ Aliucord/src/main/java/com/aliucord/widgets/PluginCard.java
@@ -1,10 +1,10 @@
 /*
- * This file is part of Aliucord, an Android Discord client mod.
+ * This file is part of DHCord, an Android Discord client mod.
  * Copyright (c) 2021 Juby210 & Vendicated
  * Licensed under the Open Software License version 3.0
  */
 
-package com.aliucord.widgets;
+package com.dhcord.widgets;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -17,9 +17,9 @@ import android.widget.TextView;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 
-import com.aliucord.Constants;
-import com.aliucord.utils.DimenUtils;
-import com.aliucord.views.*;
+import com.dhcord.Constants;
+import com.dhcord.utils.DimenUtils;
+import com.dhcord.views.*;
 import com.discord.utilities.color.ColorCompat;
 import com.discord.views.CheckedSetting;
 import com.google.android.material.card.MaterialCardView;
