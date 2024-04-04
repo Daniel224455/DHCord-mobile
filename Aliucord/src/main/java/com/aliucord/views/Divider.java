9eda28335eb819a8777ce21ff1781b1ba2b2909e
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-04-04 20:08:30 +0200

    dhc

--- Aliucord/src/main/java/com/aliucord/views/Divider.java
+++ Aliucord/src/main/java/com/aliucord/views/Divider.java
@@ -1,16 +1,16 @@
 /*
- * This file is part of Aliucord, an Android Discord client mod.
+ * This file is part of dhcord, an Android Discord client mod.
  * Copyright (c) 2021 Juby210 & Vendicated
  * Licensed under the Open Software License version 3.0
  */
 
-package com.aliucord.views;
+package com.dhcord.views;
 
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 
-import com.aliucord.utils.DimenUtils;
+import com.dhcord.utils.DimenUtils;
 import com.discord.utilities.color.ColorCompat;
 import com.lytefast.flexinput.R;
 
