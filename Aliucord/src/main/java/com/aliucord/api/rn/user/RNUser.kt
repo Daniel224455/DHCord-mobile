ac7a31bf36878d8444482d28457fcbb7240c74da
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-04-04 16:27:38 +0200

    dhc

--- Aliucord/src/main/java/com/aliucord/api/rn/user/RNUser.kt
+++ Aliucord/src/main/java/com/aliucord/api/rn/user/RNUser.kt
@@ -4,7 +4,7 @@
  * Licensed under the Open Software License version 3.0
  */
 
-package com.aliucord.api.rn.user
+package com.dhcord.api.rn.user
 
 import com.discord.api.guildmember.GuildMember
 import com.discord.api.premium.PremiumTier
