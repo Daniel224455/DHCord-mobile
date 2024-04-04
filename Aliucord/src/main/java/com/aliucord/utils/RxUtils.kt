3c5b1a591432e42db684f58daa9346cc97ca5bd5
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-04-04 20:06:38 +0200

    dhc

--- Aliucord/src/main/java/com/aliucord/utils/RxUtils.kt
+++ Aliucord/src/main/java/com/aliucord/utils/RxUtils.kt
@@ -1,10 +1,10 @@
 /*
- * This file is part of Aliucord, an Android Discord client mod.
+ * This file is part of dhcord, an Android Discord client mod.
  * Copyright (c) 2021 Juby210 & Vendicated
  * Licensed under the Open Software License version 3.0
  */
 
-package com.aliucord.utils
+package com.dhcord.utils
 
 import android.os.Looper
 import j0.l.a.c
